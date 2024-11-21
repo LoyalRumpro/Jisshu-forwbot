081d96626f97ff83a59c5b1e2aa59582c0418eb3
Ravandeadlya <Ravandeadly@outlook.com>
2024-11-21 19:58:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100'))
+    #LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100'))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002288562750'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/yrdvji64") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
